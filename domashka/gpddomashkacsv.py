13f1e7d18a07069ea21d07cf9f2bca2cddf7930a
ALEKSANDR USOVV <shyraus@gmail.com>
2023-02-04 21:33:47 +0200

    ya

--- domashka/gpddomashkacsv.py
+++ domashka/gpddomashkacsv.py
@@ -7,8 +7,23 @@ def sorting_by_gpd(file_path):
 
     return sorted_rows
 
-file_path = "2019.csv"
+# file_path = "2019.csv"
+# sorted_countries = sorting_by_gpd(file_path)
+
+# for country in sorted_countries:
+#     print(country["Country or region"], country["GDP per capita"])
+
+def sorting_by_corr(sorted_countries):
+    sorted_by_corruption = sorted(sorted_countries, key=lambda row: float(row["Perceptions of corruption"]))
+    print("Top 5 counties by Perceptions of corruption: ")
+    for i in range(5):
+        print(f'{i+1}.{sorted_by_corruption[i]["Country or region"]} - Corruption level: {sorted_by_corruption[i]["Perceptions of corruption"]}')
+
+file_path = '2019.csv'
 sorted_countries = sorting_by_gpd(file_path)
 
+print('Coutries sorted by GDP by capita: ')
 for country in sorted_countries:
     print(country["Country or region"], country["GDP per capita"])
+
+sorting_by_corr(sorted_countries)
